51274173e709b0e8dc47930e48ca9d862b8efa7f
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2026-01-15 20:44:53 -0500

    added path planner

---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,135 @@
+package frc.robot.subsystems;
+
+import java.util.Optional;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import frc.robot.generated.TunerConstants;
+import limelight.Limelight;
+import limelight.networktables.LimelightPoseEstimator;
+import limelight.networktables.LimelightSettings.ImuMode;
+import limelight.networktables.LimelightSettings.LEDMode;
+import limelight.networktables.PoseEstimate;
+
+public class Drive extends CommandSwerveDrivetrain {
+    private final double stickDeadband = 0.1; // configurable deadband for controller
+    private final double maxAngularRate = Units.RotationsPerSecond.of(0.75).in(Units.RadiansPerSecond); // configure the maximum rotational velocity in teleoperated mode.
+
+    private final SwerveRequest.FieldCentric teleopRequest = new SwerveRequest.FieldCentric()
+        .withDeadband(TunerConstants.kSpeedAt12Volts.in(Units.MetersPerSecond) * 0.1).withRotationalDeadband(maxAngularRate * stickDeadband) // Add a 10% deadband
+        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    
+    private final SwerveRequest.ApplyRobotSpeeds m_pathApplyRobotSpeeds = new SwerveRequest.ApplyRobotSpeeds();
+    private Limelight limelight_bob;
+    private LimelightPoseEstimator bobEstimator;
+
+
+    // Instantiate a new instance of the Drive subsystem.
+    public Drive() {
+        super(TunerConstants.DrivetrainConstants, TunerConstants.FrontLeft, TunerConstants.FrontRight, TunerConstants.BackLeft, TunerConstants.BackRight);
+        try {
+            var config = RobotConfig.fromGUISettings();
+            AutoBuilder.configure(
+                () -> getState().Pose,   // Supplier of current robot pose
+                this::resetPose,         // Consumer for seeding pose against auto
+                () -> getState().Speeds, // Supplier of current robot speeds
+                // Consumer of ChassisSpeeds and feedforwards to drive the robot
+                (speeds, feedforwards) -> setControl(
+                    m_pathApplyRobotSpeeds.withSpeeds(ChassisSpeeds.discretize(speeds, 0.020))
+                        .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
+                        .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
+                ),
+                new PPHolonomicDriveController(
+                    // PID constants for translation
+                    new PIDConstants(10, 0, 0),
+                    // PID constants for rotation
+                    new PIDConstants(7, 0, 0)
+                ),
+                config, 
+                // Assume the path needs to be flipped for Red vs Blue, this is normally the
+                // case
+                () -> {
+                        var alliance = DriverStation.getAlliance();
+                        if (alliance.isPresent()) {
+                            return alliance.get() == DriverStation.Alliance.Red;
+                        }
+                        return false;
+                    },
+                    this
+            );
+        } catch (Exception ex) {
+            DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
+        }
+
+        // Instantiate first limelight.
+        //TODO: Test if it can handle disconnects. Instantiate a nonexistent limelight to check.
+
+        limelight_bob = new Limelight("limelight-bob");
+        
+        limelight_bob.getSettings()
+          .withLimelightLEDMode(LEDMode.PipelineControl)
+          .withCameraOffset(new Pose3d(
+              Units.Inches.of(-11.00).in(Units.Meters),
+              Units.Inches.of(9.0).in(Units.Meters),
+              Units.Inches.of(12.75).in(Units.Meters),
+              new Rotation3d(0, 0, 0))) // report in radians if necessary
+          .withImuMode(ImuMode.InternalImuMT1Assist)
+          .withImuAssistAlpha(0.01)
+          .save();
+
+        RobotModeTriggers.disabled().onTrue(Commands.runOnce(() -> {
+        System.out.println("Neutralizing IMU Assist");
+
+        limelight_bob.getSettings()
+            // .withImuMode(ImuMode.InternalImuMT1Assist)
+            .withImuAssistAlpha(0.001)
+            .save();
+      }).ignoringDisable(true));
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+        Optional<PoseEstimate> visionEstimate = bobEstimator.getPoseEstimate();
+
+      // If the pose is present
+      visionEstimate.ifPresent((PoseEstimate poseEstimate) -> {
+        if (poseEstimate.tagCount > 0) {
+          SmartDashboard.putNumber("Limelight/Megatag2Count", poseEstimate.tagCount);          
+
+          Pose3d redHub = new Pose3d(
+              Units.Meters.of(11.902),
+              Units.Meters.of(4.031),
+              Units.Meters.of(0.0),
+              new Rotation3d(0, 0, 0));
+
+          double distanceToHub = poseEstimate.pose.toPose2d().minus(redHub.toPose2d()).getTranslation().getNorm();
+
+          SmartDashboard.putNumber("FieldSimulation/hubDiff", distanceToHub);
+
+          // Add it to the pose estimator.
+          super.addVisionMeasurement(
+              poseEstimate.pose.toPose2d(),
+              poseEstimate.timestampSeconds);
+
+          super.setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
+
+        }
+      });
+    }
+}
+
