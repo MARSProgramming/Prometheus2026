18a4b13ea66c1ded9f5011ef20216a41a96fa823
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2026-01-31 09:44:59 -0500

    update Choreo Config

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 
 import java.util.Optional;
 
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
